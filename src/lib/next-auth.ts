c6cac0269ca0490a026e8fd2acfca5a70d57cb48
Quocbao <baotq310@gmail.com>
2025-07-05 10:09:27 +0700

    refactor: Standardize import statements and improve global styles

--- src/lib/next-auth.ts
+++ src/lib/next-auth.ts
@@ -1,52 +1,59 @@
-import { DefaultSession, NextAuthOptions } from "next-auth";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { prisma } from "@/lib/PrismaClient";
-import GoogleProvider from "next-auth/providers/google";
+import {
+  DefaultSession,
+  getServerSession,
+  NextAuthOptions,
+} from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from '@/lib/PrismaClient';
+import GoogleProvider from 'next-auth/providers/google';
 
-declare module "next-auth" {
-    interface Session {
-        user: {
-            id: string;
-        } & DefaultSession["user"];
-    }
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession['user'];
+  }
 }
 
-
 const authOptions: NextAuthOptions = {
-    session: {
-        strategy: "jwt",
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async jwt({ token }) {
+      if (token.email) {
+        const user = await prisma.user.findUnique({
+          where: {
+            email: token.email,
+          },
+        });
+        if (user) {
+          token.id = user.id;
+        }
+      }
+      return token;
     },
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        async jwt({ token }) {
-            if (token.email) {
-                const user = await prisma.user.findUnique({
-                    where: {
-                        email: token.email,
-                    },
-                });
-                if (user) {
-                    token.id = user.id;
-                }
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            if (session.user) {
-                session.user.id = token.id as string;
-                session.user.name = token.name as string;
-                session.user.email = token.email as string;
-                session.user.image = token.image as string;
-            }
-            return session;
-        },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.image = token.image as string;
+      }
+      return session;
     },
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        }),
-    ],
+  },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
 };
 export default authOptions;
+
+export const getAuthSession = async () => {
+  return await getServerSession(authOptions);
+};
