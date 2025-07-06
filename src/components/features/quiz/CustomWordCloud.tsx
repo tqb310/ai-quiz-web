a582bae9b60c88512648d3e53db8b3b5bae3af65
Quocbao <baotq310@gmail.com>
2025-07-06 17:44:52 +0700

    fix: added dynamic import for D3WordCloud

--- src/components/features/quiz/CustomWordCloud.tsx
+++ src/components/features/quiz/CustomWordCloud.tsx
@@ -1,8 +1,14 @@
 'use client';
 import React from 'react';
-import D3WordCloud from 'react-d3-cloud';
+// import D3WordCloud from 'react-d3-cloud';
 import data from '@/constants/D3WordCloudData';
 import { useTheme } from 'next-themes';
+import dynamic from 'next/dynamic';
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('react-d3-cloud'),
+  { ssr: false }
+);
 
 type Props = {};
 
@@ -10,7 +16,7 @@ const CustomWordCloud = (props: Props) => {
   const { theme } = useTheme();
   return (
     <>
-      <D3WordCloud
+      <DynamicComponentWithNoSSR
         data={data}
         height={350}
         font="Times"
