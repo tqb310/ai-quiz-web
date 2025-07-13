ed9f108586ffa73cc1d36730fa8ffc68c0ddc371
Quocbao <baotq310@gmail.com>
2025-07-13 23:07:35 +0700

    feat: Add date-fns for date manipulation and enhance MCQ component
    - Added date-fns library for improved date handling.
    - Introduced time formatting functionality in the MCQ component to display elapsed time.
    - Implemented a new utility function to format time deltas for better user experience.
    - Enhanced the MCQ component with a timer and completion message upon quiz finish.

--- src/components/features/quiz/MCQ.tsx
+++ src/components/features/quiz/MCQ.tsx
@@ -1,18 +1,31 @@
 'use client';
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import { Game, Question } from '@prisma/client';
-import { ChevronRight, Loader2, Timer } from 'lucide-react';
+import {
+  BarChart,
+  ChevronRight,
+  Loader2,
+  Timer,
+} from 'lucide-react';
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import {
+  Button,
+  buttonVariants,
+} from '@/components/ui/button';
 import MCQCounter from './MCQCounter';
 import { useMutation } from '@tanstack/react-query';
 import { questionAPI } from '@/services/question.api';
 import { toast } from 'sonner';
+import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { differenceInSeconds } from 'date-fns';
+import { formatTimeDelta } from '@/lib/utils';
+
 type Props = {
   game: {
     questions: Pick<
@@ -29,6 +42,15 @@ function MCQ({ game }: Props) {
   >(null);
   const [correctAnswers, setCorrectAnswers] = useState(0);
   const [wrongAnswers, setWrongAnswers] = useState(0);
+  const [ended, setEnded] = useState(false);
+  const [now, setNow] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!ended) setNow(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [ended]);
 
   const currentQuestion = useMemo(() => {
     return game.questions[questionIndex];
@@ -51,12 +73,11 @@ function MCQ({ game }: Props) {
       if (data.isCorrect) {
         setCorrectAnswers(correctAnswers + 1);
         toast.success('Correct answer!', {
-          
           className:
             'flex flex-row items-center gap-2 px-4 py-2 rounded-md shadow-lg bg-green-500 text-white',
           description: 'You are a genius!',
           position: 'top-center',
-          duration: 60000,
+          duration: 1000,
           unstyled: true,
           classNames: {
             title: 'font-bold',
@@ -69,7 +90,7 @@ function MCQ({ game }: Props) {
             'flex flex-row items-center gap-2 px-4 py-2 rounded-md shadow-lg bg-red-500 text-white',
           description: 'Do not give up! You can do it!',
           position: 'top-center',
-          duration: 30000,
+          duration: 1000,
           unstyled: true,
           classNames: {
             title: 'font-bold',
@@ -79,12 +100,21 @@ function MCQ({ game }: Props) {
       if (game.questions.length > questionIndex + 1) {
         setQuestionIndex(questionIndex + 1);
         setSelectedChoice(null);
+      } else if (
+        game.questions.length ===
+        questionIndex + 1
+      ) {
+        setEnded(true);
       }
     },
     onError: (error) => {
       console.error(error);
     },
   });
+
+  const isLastQuestion = useMemo(() => {
+    return questionIndex + 1 === game.questions.length;
+  }, [questionIndex, game.questions]);
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-[80vw] max-w-4xl w-[90vw]">
@@ -97,70 +127,89 @@ function MCQ({ game }: Props) {
         </p>
         <div className="flex items-center gap-2 text-slate-400">
           <Timer />
-          <span>00:00</span>
+          <span>
+            {formatTimeDelta(
+              differenceInSeconds(now, game.timeStarted)
+            )}
+          </span>
         </div>
         <MCQCounter
           correctAnswers={correctAnswers}
           wrongAnswers={wrongAnswers}
         />
       </div>
-      <Card className="w-full mt-4">
-        <CardHeader className="flex flex-row items-center">
-          <CardTitle className="mr-5 text-center divide-y divide-zinc-600/50">
-            <div className="text-base">
-              {questionIndex + 1}
-            </div>
-            <div className="text-base text-slate-400">
-              {game.questions.length}
-            </div>
-          </CardTitle>
-          <CardDescription className="grow text-lg">
-            {currentQuestion.question}
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <div className="flex flex-col items-center justify-center w-full mt-4 gap-4">
-        {currentOptions.map((option, index) => (
-          <Button
-            key={index}
-            className="w-full cursor-pointer py-8"
-            onClick={() => setSelectedChoice(option)}
-            variant={
-              selectedChoice === option
-                ? 'default'
-                : 'secondary'
-            }
+      {ended ? (
+        <div className="absolute-center flex flex-col items-center justify-center w-full mt-4 gap-4">
+          <h1 className="px-4 mt-2 font-semibold text-white bg-green-500 rounded-md whitespace-nowrap">
+            You completed in "3m 4s"
+          </h1>
+          <Link
+            href="/quiz/statistics"
+            className={cn(buttonVariants(), 'mt-2')}
           >
-            <div className="flex items-center justify-start w-full">
-              <div className="p-2 px-3 mr-5 border rounded-md">
-                {index + 1}
-              </div>
-              <div className="text-start">{option}</div>
-            </div>
-          </Button>
-        ))}
-        <Button
-          className="mt-4"
-          disabled={isPending}
-          onClick={() => {
-            checkAnswer({
-              questionId: currentQuestion?.id || '',
-              userAnswer: selectedChoice || '',
-            });
-          }}
-        >
-          {isPending ? (
-            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-          ) : (
-            <>
-              {game.questions.length === questionIndex + 1
-                ? 'Finish'
-                : 'Next'}
-              <ChevronRight className="w-4 h-4 ml-2" />
-            </>
-          )}
-        </Button>
-      </div>
+            View Statistics
+            <BarChart className="w-4 h-4 ml-2" />
+          </Link>
+        </div>
+      ) : (
+        <>
+          <Card className="w-full mt-4">
+            <CardHeader className="flex flex-row items-center">
+              <CardTitle className="mr-5 text-center divide-y divide-zinc-600/50">
+                <div className="text-base">
+                  {questionIndex + 1}
+                </div>
+                <div className="text-base text-slate-400">
+                  {game.questions.length}
+                </div>
+              </CardTitle>
+              <CardDescription className="grow text-lg">
+                {currentQuestion.question}
+              </CardDescription>
+            </CardHeader>
+          </Card>
+          <div className="flex flex-col items-center justify-center w-full mt-4 gap-4">
+            {currentOptions.map((option, index) => (
+              <Button
+                key={index}
+                className="w-full cursor-pointer py-8"
+                onClick={() => setSelectedChoice(option)}
+                variant={
+                  selectedChoice === option
+                    ? 'default'
+                    : 'secondary'
+                }
+              >
+                <div className="flex items-center justify-start w-full">
+                  <div className="p-2 px-3 mr-5 border rounded-md">
+                    {index + 1}
+                  </div>
+                  <div className="text-start">{option}</div>
+                </div>
+              </Button>
+            ))}
+            <Button
+              className="mt-4"
+              disabled={isPending}
+              onClick={() => {
+                checkAnswer({
+                  questionId: currentQuestion?.id || '',
+                  userAnswer: selectedChoice || '',
+                });
+              }}
+            >
+              {isPending ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                <>
+                  {isLastQuestion ? 'Finish' : 'Next'}
+                  <ChevronRight className="w-4 h-4 ml-2" />
+                </>
+              )}
+            </Button>
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatTimeDelta(delta: number) {
+  const hours = Math.floor(delta / 3600);
+  const minutes = Math.floor((delta - hours * 3600) / 60);
+  const remainingSeconds = delta % 60;
+  const parts = [];
+  if (hours > 0) {
+    parts.push(`${hours}h`);
+  }
+  if (minutes > 0) {
+    parts.push(`${minutes}m`);
+  }
+  if (remainingSeconds > 0) {
+    parts.push(`${remainingSeconds}s`);
+  }
+  return parts.join(' ');
 }
